32218cdfe9ec3380d48158cea1ae43edc7ba72ed
Kumaranraja <kumaranraja46248@gmail.com>
2025-10-24 20:17:51 +0530

    hide create database button

---
+++ addons/custom_addons/db_manager_hide_create/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Hide Create Database Button",
+    "version": "1.0",
+    "category": "Tools",
+    "summary": "Hide the Create Database button from the manager page",
+    "author": "Your Name",
+    "depends": ["web"],
+    "data": [
+        "views/db_manager_hide_create.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
